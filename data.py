d52e3bb8bd6ead6b9eec1d158300ff4ee12dd2e6
lynchej <lynchej>
2025-06-10 17:13:31 -0400

    working on adding the data into the database. Stuck on building the correct querey. Need to check over each column in the file we are looking at is in the sql if not set them to null.

---
+++ data.py
@@ -0,0 +1,51 @@
+from pydoc import describe
+
+import mysql.connector
+import pandas as pd
+
+mydb=mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='Elhockey123',
+    port='3306',
+    database='hockey_db'
+
+
+)
+#cursor for adding data to database
+cursor = mydb.cursor()
+
+csv_columns=''
+total_cols
+#the column names gotten from the most recent csv
+columns= pd.read_csv('./data/hockey_stat2025.csv').columns
+for col in columns:
+    csv_columns=csv_columns+ ', ' + col.lower()
+csv_columns=csv_columns+', year'
+csv_columns=csv_columns[2:]
+print(csv_columns)
+
+# the sql column names
+cursor.execute("SHOW COLUMNS FROM player_stats")
+sql_columns = [row[0] for row in cursor.fetchall()]
+print(sql_columns)
+
+
+#look at every csv
+for year in range(1950, 2026):
+    try:
+        df = pd.read_csv(f'./data/hockey_stat{year}.csv')
+        for row in range(len(df)):
+            row_data=''
+            for i in df.iloc[row]:
+                #need to check if each has the right columns if not set value to null
+                row_data=row_data+','+ str(i)
+            print(f'INSERT INTO player_stats ({csv_columns}) values ({row_data[1:] +"," +str(year)})')
+            cursor.execute(f'INSERT INTO player_stats ({csv_columns}) values ({row_data[1:] +"," +str(year)})')
+
+
+    except FileNotFoundError:
+        print(f"{year}: File not found")
+
+
+
